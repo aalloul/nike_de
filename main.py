08b1fa55a79f28eadb360dd3a31450428f0f50d8
aa01163 <adam.alloul@randstadgroep.nl>
2021-07-10 19:06:06 +0200

    add weekly_aggregator.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from calendar_data_reader import CalendarDataReader
 from product_data_reader import ProductDataReader
 from sales_data_reader import SalesDataReader
 from store_data_reader import StoreDataReader
+from weekly_aggregator import WeeklyAggregator
 from config import Config
 from pyspark.sql import SparkSession
 
@@ -15,3 +16,6 @@ if __name__ == "__main__":
     sales_df = SalesDataReader(spark, Config).read()
     product_df = ProductDataReader(spark, Config).read()
     store_df = StoreDataReader(spark, Config).read()
+
+    aggregated_sales = WeeklyAggregator(calendar_df, sales_df, product_df, store_df).aggregate_per_week()
+
---
+++ src/aggregators/weekly_aggregator.py
@@ -0,0 +1,37 @@
+from pyspark.sql import functions as F, dataframe
+
+
+class WeeklyAggregator(object):
+    def __init__(self, calendar: dataframe, sales: dataframe, product: dataframe, store: dataframe):
+        self.calendar = calendar
+        self.sales = sales
+        self.product = product
+        self.store = store
+
+    def _build_full_df(self) -> dataframe:
+        """
+        This method builds a `pyspark.sql.dataframe` that contains all the possible sales on the website by creating a
+        `crossJoin` between `product`, `calendar` and `store` dataframes
+        :return: A dataframe.
+        """
+
+        return self.product.crossJoin(self.calendar).crossJoin(self.store)
+
+    def aggregate_per_week(self) -> dataframe:
+        """
+        This method builds the weekly aggregation based on the input sales data.
+        :return:
+        """
+
+        all_possible_sales = self._build_full_df()
+
+        # `fillna(0)` is chosen because a `np.nan` simply means that there was no sales for the tuple
+        # `dateId, store_id, productId`
+        actual_sales = all_possible_sales \
+            .join(self.sales, ["dateId", "storeId", "productId"], how="left") \
+            .fillna(0)
+
+        return actual_sales \
+            .groupBy("division", "gender", "category", "channel", "datecalendaryear", "weekNumber") \
+            .agg(F.sum("netSales").alias("totalNetSales"),
+                 F.sum("salesUnits").alias("totalSalesUnits"))
